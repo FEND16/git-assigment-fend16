fb5da953456770e13e4f76293b9db99a248119f0
magnusohrstrom <magnusohrstrom@Magnuss-MacBook-Pro-2.local>
2016-10-17 11:18:39 +0200

    Uppgift inlämnad/Magnus

---
+++ magnus_ohrstrom_busfare/magnus_ohrstrom_busfare.js
@@ -0,0 +1,48 @@
+//Busfare Js
+
+//Funktion för att fylla på saldo på resbiljett
+
+function buyTicket(){
+
+//Resenären anger resans kostnad. (Prompt)
+var cost = prompt("How much is your travel fee?: ");
+cost = Number(cost);
+
+//Ticket saldo.
+var ticket = 0;
+
+//Antal transaktioner.
+var trans = 0;
+
+//Loop som körs tills att saldot för kortet är tillräckligt stort för att täcka reskostnad.
+//Kortet ska fyllas på med så få transaktioner som möjligt.
+//Programmet kan göra tre olika transaktioner. 500 , 200 samt 100 kr. Transaktionssumman
+// ska i slutändan överstiga kostnaden så lite som möjligt. 
+//Kostnaden för resan får inte överstiga 10000 kr. 
+while(cost > ticket&& cost <=10000)
+	{	//Programmet kan göra tre olika transaktioner. 500 , 200 samt 100 kr. Transaktionssumman
+		// ska i slutändan överstiga kostnaden så lite som möjligt. 
+	if (ticket + 500 <= (cost + 50)) 
+			{ 
+				ticket += 500; 
+			}
+	
+	else if (ticket + 200 <= (cost+ 50 ))
+			{
+				ticket += 200;
+			}
+
+	else if (ticket + 100 <= (cost + 50))
+			{
+				ticket +=  100;
+			}
+	trans ++;
+	}
+	//returnerar antal transaktioner.
+	return "Antal transaktioner: " + trans;
+};
+// Skriver ut saldo som funktionen returnerar. 
+console.log(buyTicket());
+
+
+
---
+++ magnus_ohrstrom_busfare/magnus_ohrstrom_busfare.md
@@ -0,0 +1,35 @@
+Busfare!
+
+###Pseudokod där jag tar mig an komunaltrafikens förunderliga värld:
+
+```
+Start
+
+Funktion som räknar ut hur många transaktioner som görs när en resenär behöver fylla på
+sin biljett för en viss reskostnad
+
+Resenären anger resans kostnad. Den får inte överstiga 10000 kr. 
+
+Antalet transaktioner är från början = 0.
+Biljettsaldo är från början = 0.
+
+Reskortet kan fyllas på med summorna 500, 200 resp 100 kr.
+Vi vill att programmet ska genomföra så få transaktioner som möjligt samt att slutsaldot 
+på biljetten ska överskrida reskostnaden med så lite som möjligt. 
+
+Loop som adderar pengar till resenärens biljett.
+WHILE(Resans kostnad är större än Kortsaldo och kostnaden är mindre än 10000)
+	IF  (kortsaldo + 500 är större eller lika med Resans kostnad + 50) 
+			kortsaldo= korsaldo +500 
+	
+	ELSE IF (Kortsaldo + 200 <= (Resekostnad + 50 ))
+			kortsaldo = kortsaldo + 200
+
+	ELSE IF (kortsaldo + 100 <= (resekostnad + 50))
+			kortsaldo = kortsaldo + 100
+      
+      Varje gång loopen körs räknas antalet transaktioner upp med 1. 
+
+Funktionen returnerar antalet transaktioner. 
+
+Slut
