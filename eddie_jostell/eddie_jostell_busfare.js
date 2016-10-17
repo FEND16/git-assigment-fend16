04673ab01c4dd9d7066ef87659bf388884926c00
Eddie Jostell <eddie.jostell@gmail.com>
2016-10-17 16:22:57 +0200

    New thinking

--- eddie_jostell/eddie_jostell_busfare.js
+++ eddie_jostell/eddie_jostell_busfare.js
@@ -38,3 +38,42 @@ var count = 0;
 busFare(850);*/
 
 
+var travelCost = prompt("Hur mycket vill du resa för?");
+
+function busFare(cost) {
+
+    var transactions = 0;
+
+    var fiveHundred = 500;
+    var twoHundred = 200;
+    var oneHundred = 100;
+
+    if (travelCost > 10000) {
+        return console.log("Maxbelopp är 10000 starta om och försök igen");
+    }
+
+    if(Math.floor(travelCost / fiveHundred) !== 0){
+      transactions = Math.floor(travelCost / fiveHundred);
+      travelCost -= fiveHundred * transactions;
+      console.log(travelCost);
+
+      if(Math.floor(travelCost / twoHundred) !== 0){
+        var intermediateTransaction = Math.floor(travelCost / twoHundred);
+        transactions += intermediateTransaction;
+        travelCost -= twoHundred * intermediateTransaction;
+        console.log(travelCost);
+      }
+
+      if(Math.floor(travelCost / oneHundred) !== 0){
+        var intermediateTransaction = Math.floor(travelCost / oneHundred);
+        transactions += intermediateTransaction;
+        travelCost -= oneHundred * intermediateTransaction;
+        console.log(travelCost);
+      }
+
+    }
+    console.log('Rest av travelCost '+ travelCost);
+    console.log('Antal transaktioner: '+ transactions);
+  }
+
+busFare();
